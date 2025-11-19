b7536e43b1ac78e3b549071b3bd2f95947801abb
asraful-07 <asraful0508@gmail.com>
2025-11-19 09:18:17 +0600

    explore typescript

--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,19 @@
-const str: string = "hello world";
+class Person {
+  private name: string;
+  private age: number;
 
-console.log(str);
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getDetails(): string {
+    return `'Name: ${this.name}, Age: ${this.age}'`;
+  }
+}
+
+const person1 = new Person("John Doe", 30);
+console.log(person1.getDetails());
+
+const person2 = new Person("Alice", 25);
+console.log(person2.getDetails());
